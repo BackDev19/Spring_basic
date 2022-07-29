882aaa9a633edd1916675d18d11b7979d98dbfb7
BackDev19 <ghkdrnjs180@naver.com>
2022-07-29 19:52:34 +0900

    4주차 숙제

--- week04/src/main/java/com/sparta/week04/controller/ProductRestController.java
+++ week04/src/main/java/com/sparta/week04/controller/ProductRestController.java
@@ -1,13 +1,12 @@
 package com.sparta.week04.controller;
 
 import com.sparta.week04.models.Product;
+import com.sparta.week04.models.ProductMyPriceRequestDto;
 import com.sparta.week04.models.ProductRepository;
 import com.sparta.week04.models.ProductRequestDto;
+import com.sparta.week04.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,6 +14,7 @@ import java.util.List;
 @RestController // JSON으로 데이터를 주고받음을 선언합니다.
 public class ProductRestController {
 
+    private final ProductService productService;
     private final ProductRepository productRepository;
 
     // 등록된 전체 상품 목록 조회
@@ -23,9 +23,17 @@ public class ProductRestController {
         return productRepository.findAll();
     }
 
+    // 신규 상품 등록
     @PostMapping("/api/products")
     public Product createProduct(@RequestBody ProductRequestDto requestDto) {
         Product product = new Product(requestDto);
-        return productRepository.save(product);
+        productRepository.save(product);
+        return product;
+    }
+
+    // 설정 가격 변경
+    @PutMapping("/api/products/{id}")
+    public Long updateProduct(@PathVariable Long id, @RequestBody ProductMyPriceRequestDto requestDto) {
+        return productService.update(id, requestDto);
     }
 }
