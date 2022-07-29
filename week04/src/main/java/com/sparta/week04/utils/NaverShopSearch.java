2652775d396a25920ec672bc8decd728fef1659f
BackDev19 <ghkdrnjs180@naver.com>
2022-07-29 19:49:05 +0900

    4-9 [4주차] 키워드로 상품 검색하기 - 네이버 API와 서비스 연결하기

--- week04/src/main/java/com/sparta/week04/utils/NaverShopSearch.java
+++ week04/src/main/java/com/sparta/week04/utils/NaverShopSearch.java
@@ -4,17 +4,19 @@ import com.sparta.week04.models.ItemDto;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.http.*;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component // 이제부터, @RequiredArgsConstructor 와 함께 사용할 경우 스프링이 자동으로 생성합니다.
 public class NaverShopSearch {
     public String search(String query) {
         RestTemplate rest = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
-        headers.add("X-Naver-Client-Id", "m3t2WFZlSeAGawfGeUpF");
-        headers.add("X-Naver-Client-Secret", "CLOi7ievQw");
+        headers.add("X-Naver-Client-Id", "zdqMoIkFaK8uKvC2oNY2");
+        headers.add("X-Naver-Client-Secret", "LiZfsgtuD5");
         String body = "";
 
         HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
@@ -30,23 +32,13 @@ public class NaverShopSearch {
 
     public List<ItemDto> fromJSONtoItems(String result) {
         JSONObject rjson = new JSONObject(result);
-        JSONArray items = rjson.getJSONArray("items");
-
-        List<ItemDto> itemDtoList = new ArrayList<>();
-
-
-        for (int i = 0; i < items.length(); i++) {
-            JSONObject itemJson = items.getJSONObject(i);
+        JSONArray items  = rjson.getJSONArray("items");
+        List<ItemDto> ret = new ArrayList<>();
+        for (int i=0; i<items.length(); i++) {
+            JSONObject itemJson = (JSONObject) items.get(i);
             ItemDto itemDto = new ItemDto(itemJson);
-            itemDtoList.add(itemDto);
+            ret.add(itemDto);
         }
-        return itemDtoList;
+        return ret;
     }
-
-    public static void main(String[] args) {
-        NaverShopSearch naverShopSearch = new NaverShopSearch();
-        String result = naverShopSearch.search("아이맥");
-    }
-
-
 }
